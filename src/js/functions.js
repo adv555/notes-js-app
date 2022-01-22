91c7eaba2631c6a57a00607bf97727fb079e82aa
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2022-01-22 02:22:24 +0200

    add  render markup & functions

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,14 +1,80 @@
 import tplNote from '../templates/note.hbs';
-import notes from '../notes.json';
+import tplStats from '../templates/stats.hbs';
 
-let data = {
-  notes,
-};
-console.log(data);
-// получаем заметки
+let notes = [
+  {
+    id: '1',
+    name: 'Shopping list',
+    created: 'April 27,2021',
+    category: 'Task',
+    content: 'tomato, apple, juice',
+    dates: [],
+    archived: true,
+  },
+  {
+    id: '2',
+    name: 'The theory of evolution',
+    created: 'April 27,2021',
+    category: 'Random Thought',
+    content: 'need to purchase',
+    dates: [],
+    archived: false,
+  },
+  {
+    id: '3',
+    name: 'New Feature',
+    created: 'May 05,2021',
+    category: 'Idea',
+    content: 'deadline 05-06-2021',
+    dates: ['05-06-2021'],
+    archived: true,
+  },
+  {
+    id: '4',
+    name: 'William Gadis',
+    created: 'May 07,2021',
+    category: 'Random Thought',
+    content: 'deadline 05-06-2021, 06-06-2021',
+    dates: ['05-06-2021', '06-06-2021'],
+    archived: false,
+  },
+  {
+    id: '5',
+    name: 'Books',
+    created: 'May 15,2021',
+    category: 'Random Thought',
+    content: "Don't forget to read if you bought",
+    dates: [],
+    archived: false,
+  },
+  {
+    id: '6',
+    name: 'Just new task',
+    created: 'May 15,2021',
+    category: 'Task',
+    content: 'Get ready for a new tasks',
+    dates: [],
+    archived: false,
+  },
+  {
+    id: '7',
+    name: 'Random Thought',
+    created: 'May 15,2021',
+    category: 'Idea',
+    content: 'Create somethig...',
+    dates: [],
+    archived: false,
+  },
+];
 
-export function getNotes(notes) {
-  return notes.map(tplNote).join('');
+console.log(notes);
+
+// получаем активные заметки
+export function getActive() {
+  return notes
+    .filter(item => !item.archived)
+    .map(tplNote)
+    .join('');
 }
 
 // добавляем заметки
@@ -16,21 +82,65 @@ export function addNote(name, category, message) {
   const note = {
     id: Date.now().toString(),
     name: name,
-    created: new Date(),
+    created: getCreatedDate(),
     category: category,
     content: message,
     dates: getDates(message),
     archived: false,
   };
 
-  data.notes = { ...data.notes, note };
-  console.log(data.notes);
+  notes = [...notes, note];
 }
 
 // удаляем заметки
 export const deleteNote = id => {
   const newNotes = notes.filter(note => note.id !== id);
-  data.notes = newNotes;
+  return (notes = [...newNotes]);
+};
+
+// статистика
+export const getStatistics = () =>
+  notes
+    .reduce((acc, note) => {
+      const category = acc.find(cat => cat.name === note.category);
+
+      if (!category) {
+        return [
+          ...acc,
+          {
+            name: note.category,
+            active: note.archived ? 0 : 1,
+            archived: note.archived ? 1 : 0,
+          },
+        ];
+      }
+
+      note.archived ? category.archived++ : category.active++;
+      // console.log(acc);
+      return acc;
+    }, [])
+    .map(tplStats)
+    .join('');
+
+// меняем статус archived
+export const changeStatus = id => {
+  const note = notes.find(note => note.id === id);
+  // console.log(note);
+  note.archived = !note.archived;
+};
+// обновляем  заметку
+export const updateNote = (id, name, category, message) => {
+  const note = notes.find(note => note.id === id);
+
+  note.name = name;
+  note.category = category;
+  note.content = message;
+  note.dates = getDates(message);
+  console.log(note);
+};
+
+export const getNoteById = id => {
+  return notes.find(note => note.id === id);
 };
 
 // helpers
@@ -42,3 +152,9 @@ export const getDates = text => {
   }
   return results;
 };
+
+export const getCreatedDate = () =>
+  `${new Date().toLocaleString('en', {
+    month: 'long',
+    day: '2-digit',
+  })}, ${new Date().getFullYear()} `;
--- src/js/notes.js
+++ src/js/notes.js
@@ -1,44 +1,117 @@
 import refs from './refs';
-import notes from '../notes.json';
-// import tplNote from '../templates/note.hbs';
-import { getNotes, addNote } from './functions';
+import {
+  getActive,
+  addNote,
+  getNoteById,
+  updateNote,
+  deleteNote,
+  changeStatus,
+  getStatistics,
+} from './functions';
+// import { modalToggle } from './modal';
 
 //  рендерим заметки
-const notesListMarkup = getNotes(notes);
-refs.notesList.insertAdjacentHTML('afterbegin', notesListMarkup);
+const render = () => {
+  refs.notesList.innerHTML = '';
+  refs.statList.innerHTML = '';
 
-// добавляем заметки
+  refs.notesList.innerHTML = getActive();
+  refs.statList.innerHTML = getStatistics();
+};
+
+render();
+
+// добавляем и рендерим новые заметки
 refs.form.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
   const name = e.target.name.value;
   const category = e.target.category.value;
-  const message = e.target.message.value;
-  console.log(e.target.name.value);
-  console.log(e.target.category.value);
-  console.log(e.target.message.value);
+  const message = e.target.message.value.trim();
+
+  if (message === '') {
+    return alert('Fill in the note.');
+  }
+
   addNote(name, category, message);
-
-  //  const {
-  //    elements: { name, category, message },
-  //  } = e.target;
-
-  //  console.log(name.value, category.value, message.value);
-
-  //  let inps = document.querySelectorAll('input, select, textarea');
-
-  //  for (let q = 0; q < inps.length; ++q) {
-  //    if (inps[q].name && inps[q].form === this) {
-  //      console.log(typeof '%s %s', inps[q].name, inps[q].value);
-  //    }
-  //  }
+  render();
+  refs.form.reset();
 }
 
-// const notesListMarkup = createNoteListMarkup(notes);
+// находим id, удаляем заметки
+refs.notesList.addEventListener('click', onDelete);
+function onDelete(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('deleteBtn')) {
+    return;
+  }
+  const id = e.target.closest('tr').attributes.id.value;
 
-// function createNoteListMarkup(notes) {
-//   return notes.map(tplNote).join('');
-// }
+  deleteNote(id);
+  render();
+}
 
-// //  рендерим заметки
-// refs.notesList.insertAdjacentHTML('afterbegin', notesListMarkup);
+// архивируем заметки
+refs.notesList.addEventListener('click', onArchive);
+function onArchive(e) {
+  e.preventDefault();
+  if (e.target.closest('.archiveBtn')) {
+    const id = e.target.closest('tr').attributes.id.value;
+    changeStatus(id);
+    render();
+  }
+}
+// редактируем заметки
+refs.notesList.addEventListener('click', onEdit);
+function onEdit(e) {
+  e.preventDefault();
+  if (e.target.closest('.updateBtn')) {
+    const id = e.target.closest('tr').attributes.id.value;
+    console.log(id);
+
+    const note = getNoteById(id);
+    // console.log(note);
+    refs.input.value = note.name;
+    refs.select.value = '';
+    refs.textarea.value = note.content;
+    refs.form.removeEventListener('submit', onSubmit);
+    refs.form.addEventListener('submit', onSave);
+
+    function onSave(e) {
+      e.preventDefault();
+      const name = refs.input.value;
+      const category = refs.select.value;
+      const message = refs.textarea.value;
+      updateNote(id, name, category, message);
+      render();
+      refs.form.reset();
+      refs.form.removeEventListener('submit', onSave);
+      refs.form.addEventListener('submit', onSubmit);
+    }
+  }
+}
+
+refs.addBtn.addEventListener('click', modalToggle);
+
+function modalToggle() {
+  refs.modal.classList.toggle('isOpen');
+  document.addEventListener('keydown', closeEsc);
+  refs.modalBackdrop.addEventListener('click', modalBackdropClick);
+}
+
+// закрытие модалки по ESC/Click
+
+function closeEsc(e) {
+  // console.log(e.code);
+  if (e.code === 'Escape') {
+    modalToggle();
+    document.removeEventListener('keydown', closeEsc);
+  }
+}
+
+function modalBackdropClick(e) {
+  if (e.target.classList.contains('modalBackdrop')) {
+    modalToggle();
+    refs.modalBackdrop.removeEventListener('click', modalBackdropClick);
+  }
+}
